03e26ed02f561a3ac5b4105da76a3f99f6d72301
mbuiop <milad.yari1377m@gmail.com>
2026-02-18 00:05:05 +0330

    Create m1_run.py

---
+++ project/m1_run.py
@@ -0,0 +1,10 @@
+from m1_app import app, db
+from m1_models import *
+from m1_routes import *
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        print("✅ دیتابیس ساخته شد")
+    print("🚀 ربات روی http://localhost:5000 اجرا شد")
+    app.run(debug=True, port=5000)
