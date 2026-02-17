8dc1405b686b66432be08e4abf466b97800f4469
mbuiop <milad.yari1377m@gmail.com>
2026-02-18 00:01:11 +0330

    Delete project directory

--- project/m1_app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import os
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '123456789'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bot.db'
-db = SQLAlchemy(app)
