76d4d6f0ec8434eb2f008226d97290c0b6af5f9b
mbuiop <milad.yari1377m@gmail.com>
2026-02-19 15:12:38 +0330

    Update app.py

--- mega_project/app.py
+++ mega_project/app.py
@@ -14,7 +14,9 @@ from datetime import datetime
 
 # ==================== راه‌اندازی ====================
 app = Flask(__name__)
-app.config.from_object(config.Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 # دیتابیس
 db.init_app(app)
